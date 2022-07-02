9df224119fdd225afdbb9af549ab0e3e17658343
João Pedro Souza <jsouza.1650@gmail.com>
2022-07-02 03:41:55 -0300

    added comments and updateUser method at UsersController class

--- lib/src/database/controllers/users_controller.dart
+++ lib/src/database/controllers/users_controller.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:to_do_app_community/src/database/models/user.dart';
 
+/// This class is responsible for the users CRUD operations.
 class UsersController {
   FirebaseFirestore db = FirebaseFirestore.instance;
+
+  /// Register user in the database.
 
   Future<void> createUser(User user) async {
     await db.collection('users').doc(user.id).set({
@@ -12,10 +15,20 @@ class UsersController {
     });
   }
 
+  /// Get user by id from the database.
+
   Future<User> getUser(String id) async {
     final doc = await db.collection('users').doc(id).get();
     return User.fromMap(doc.data()!);
   }
+
+  /// Update user in the database.
+
+  Future<void> updateUser(User user) async {
+    await db.collection('users').doc(user.id).update(user.toMap());
+  }
+
+  /// Delete user from the database.
 
   Future<void> deleteUser(String email) async {
     await db.collection('users').doc(email).delete();

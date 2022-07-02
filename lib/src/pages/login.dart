fcb413c1fd75929bf5aebb961a63b02a8b0ae5af
João Pedro Souza <jsouza.1650@gmail.com>
2022-07-02 02:56:56 -0300

    finished LoginPage layout

--- lib/src/pages/login.dart
+++ lib/src/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app_community/src/controllers/login_page_controller/login_page_controller.dart';
 import 'package:to_do_app_community/src/pages/register.dart';
 import 'package:to_do_app_community/src/widgets/tdapc_button.dart';
 import 'package:to_do_app_community/src/widgets/tdapc_text_input.dart';
@@ -11,29 +12,40 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginPageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.init(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                'Bem Vindo!',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Bem Vindo!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 60),
-                child: form(),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 60),
+                  child: form(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -41,45 +53,50 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget form() {
-    return Column(
-      children: [
-        TDAPCTextInput(
-          'Email',
-          false,
-          width: MediaQuery.of(context).size.width * 0.8,
-          textInputType: TextInputType.emailAddress,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 20),
-          child: TDAPCTextInput(
-            'Senha',
-            true,
+    return Form(
+      key: controller.formKey,
+      child: Column(
+        children: [
+          TDAPCTextInput(
+            'Email',
+            false,
             width: MediaQuery.of(context).size.width * 0.8,
-            textInputType: TextInputType.visiblePassword,
+            textInputType: TextInputType.emailAddress,
+            controller: controller.emailController,
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 40),
-          child: TDAPCButton(
-            'Login',
-            const Color(0XFFBD8ECD),
-            () {},
-            width: MediaQuery.of(context).size.width * 0.8,
-          ),
-        ),
-        GestureDetector(
-          onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const RegisterPage(),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: TDAPCTextInput(
+              'Senha',
+              true,
+              width: MediaQuery.of(context).size.width * 0.8,
+              textInputType: TextInputType.visiblePassword,
+              controller: controller.passwordController,
             ),
           ),
-          child: const Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Text('Não possui conta? Criar uma!'),
+          Padding(
+            padding: const EdgeInsets.only(top: 40),
+            child: TDAPCButton(
+              'Login',
+              const Color(0XFFBD8ECD),
+              () => controller.login(),
+              width: MediaQuery.of(context).size.width * 0.8,
+            ),
           ),
-        )
-      ],
+          GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const RegisterPage(),
+              ),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Text('Não possui conta? Criar uma!'),
+            ),
+          )
+        ],
+      ),
     );
   }
 }

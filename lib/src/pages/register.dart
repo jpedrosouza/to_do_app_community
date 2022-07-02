0e3e2d6a06b83bbea5fa06260e56c03ede969544
João Pedro Souza <jsouza.1650@gmail.com>
2022-07-02 02:35:11 -0300

    finished RegisterPage layout

--- lib/src/pages/register.dart
+++ lib/src/pages/register.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:to_do_app_community/src/controllers/register_page_controller/register_page_controller.dart';
+import 'package:to_do_app_community/src/pages/login.dart';
 import 'package:to_do_app_community/src/widgets/tdapc_button.dart';
 import 'package:to_do_app_community/src/widgets/tdapc_text_input.dart';
 
@@ -10,40 +13,58 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final controller = RegisterPageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.init(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Faça seu cadastro!',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 60),
-                child: form(),
+      body: Observer(
+        builder: (_) => !controller.loading
+            ? SingleChildScrollView(
+                child: SafeArea(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Faça seu cadastro!',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w700),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 60),
+                          child: form(),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
               )
-            ],
-          ),
-        ),
+            : const Center(child: CircularProgressIndicator()),
       ),
     );
   }
 
   Widget form() {
     return Form(
+      key: controller.formKey,
       child: Column(
         children: [
           TDAPCTextInput(
             'Nome completo',
             false,
             width: MediaQuery.of(context).size.width * 0.8,
+            controller: controller.fullNameController,
           ),
           Padding(
             padding: const EdgeInsets.only(top: 20),
@@ -51,6 +72,7 @@ class _RegisterPageState extends State<RegisterPage> {
               'Email',
               false,
               width: MediaQuery.of(context).size.width * 0.8,
+              controller: controller.emailController,
             ),
           ),
           Padding(
@@ -59,6 +81,7 @@ class _RegisterPageState extends State<RegisterPage> {
               'Senha',
               true,
               width: MediaQuery.of(context).size.width * 0.8,
+              controller: controller.passwordController,
             ),
           ),
           Padding(
@@ -67,6 +90,7 @@ class _RegisterPageState extends State<RegisterPage> {
               'Confirme a senha',
               true,
               width: MediaQuery.of(context).size.width * 0.8,
+              controller: controller.confirmPasswordController,
             ),
           ),
           Padding(
@@ -74,12 +98,12 @@ class _RegisterPageState extends State<RegisterPage> {
             child: TDAPCButton(
               'Registre',
               const Color(0XFFBD8ECD),
-              () {},
+              () => controller.registerUser(),
               width: MediaQuery.of(context).size.width * 0.8,
             ),
           ),
           GestureDetector(
-            onTap: () {},
+            onTap: () => controller.navigateToPage(const LoginPage()),
             child: const Padding(
               padding: EdgeInsets.only(top: 20),
               child: Text('Já tem uma conta? Entre'),

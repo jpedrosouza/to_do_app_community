bf5730d3ea5324fe4577fc49753a3cfcd875927f
João Pedro Souza <jsouza.1650@gmail.com>
2022-07-01 23:31:22 -0300

    created User controller

---
+++ lib/src/database/controllers/users_controller.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:to_do_app_community/src/database/models/user.dart';
+
+class UsersController {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  Future<void> createUser(User user) async {
+    await db.collection('users').doc(user.id).set({
+      'id': user.id,
+      'email': user.email,
+      'full_name': user.fullName,
+    });
+  }
+
+  Future<Map<String, dynamic>> getUser(String id) async {
+    final doc = await db.collection('users').doc(id).get();
+    return User.fromMap(doc.data()!);
+  }
+
+  Future<void> deleteUser(String email) async {
+    await db.collection('users').doc(email).delete();
+  }
+}

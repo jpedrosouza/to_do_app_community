8b71f5c854f12f730288d4ebce15d58325585396
João Pedro Souza <jsouza.1650@gmail.com>
2022-07-02 02:40:19 -0300

    added comments on AuthService class

--- lib/src/services/auth_service.dart
+++ lib/src/services/auth_service.dart
@@ -1,19 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
+/// This class is responsible to communicate with FirebaseAuth.
+
 class AuthService {
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  /// Authenticate user with email and password.
 
   Future<UserCredential> signInWithEmailAndPassword(String email, String password) async {
     return await firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
   }
 
+  /// Create user account with email and password.
+
   Future<UserCredential> createUserWithEmailAndPassword(String email, String password) async {
     return await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
   }
 
+  /// Get authenticated user id.
+
   Future<String> getUserId() async {
     return firebaseAuth.currentUser!.uid;
   }
+
+  /// Sign out user.
 
   Future<void> signOut() async {
     return await firebaseAuth.signOut();

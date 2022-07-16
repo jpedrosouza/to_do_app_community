cfa1804f939261ac123f412d61dc459d82e2c62f
João Pedro Souza <jsouza.1650@gmail.com>
2022-07-16 03:39:51 -0300

    generate id via FirebaseFirestore case Task model dont receive id at Task class

--- lib/src/database/models/task.dart
+++ lib/src/database/models/task.dart
@@ -1,16 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Task {
-  late final String id;
+  late final String? id;
   late final String userId;
   late final String name;
   late final String frequency;
   late final DocumentReference user;
 
-  Task(this.id, this.name, this.frequency, this.userId, this.user);
+  Task(this.id, this.name, this.frequency, this.userId, this.user) {
+    id ??= FirebaseFirestore.instance.collection('tasks').doc().id;
+  }
 
   @override
-  String toString() => 'Task(id: $id, userId: $userId, name: $name, frequency: $frequency, user: $user)';
+  String toString() =>
+      'Task(id: $id, userId: $userId, name: $name, frequency: $frequency, user: $user)';
 
   static Task fromMap(Map<String, dynamic> map) {
     return Task(
